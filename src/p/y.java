660ef285a191fc851f611aad4ad5f0e4a2351c41
Lemon <s.lemon.2016@mymail.jcsu.edu>
2018-01-24 13:15:09 -0500

    first

---
+++ src/p/y.java
@@ -0,0 +1,10 @@
+package p;
+
+public class y {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/y.java
@@ -0,0 +1,12 @@
+
+public class y {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String H="Hello";
+String W="World";
+System.out.println(H+W);
+
+	}
+
+}
